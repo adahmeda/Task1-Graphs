15f6ce2c65b3169691eb29ade1c6513f507b36c1
Reihl Halle <reihalle@lfbn-1-1809-43.w90-73.abo.wanadoo.fr>
2016-11-09 19:58:45 +0200

    Initial Commit

---
+++ Software/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Software
+//
+//  Created by Rei Halle on 09/11/2016.
+//  Copyright © 2016 Rei Halle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
