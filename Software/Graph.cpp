adcbd49435d3b661b545f47dbd3cef39eb366d0b
Reihl Halle <reihalle@reis-mbp>
2016-11-09 22:22:08 +0200

    Test

---
+++ Software/Graph.cpp
@@ -0,0 +1,9 @@
+//
+//  Graph.cpp
+//  Software
+//
+//  Created by Rei Halle on 09/11/2016.
+//  Copyright © 2016 Rei Halle. All rights reserved.
+//
+
+#include "Graph.hpp"
---
+++ Software/Graph.hpp
@@ -0,0 +1,21 @@
+//
+//  Graph.hpp
+//  Software
+//
+//  Created by Rei Halle on 09/11/2016.
+//  Copyright © 2016 Rei Halle. All rights reserved.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <stdio.h>
+
+class Graph {
+public:
+    void DFS();
+    
+    
+};
+
+#endif /* Graph_hpp */
